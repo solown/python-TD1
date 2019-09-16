53ad909c03f31c32375089a641dca4194fee44f0
solown <thibaudrider@live.fr>
2019-09-16 22:26:10 +0200

    Exercice 2 OK

--- ex02/main.py
+++ ex02/main.py
@@ -4,22 +4,37 @@ import math as m
 
 class NegativeFloatExcept(Exception):
     """Exception for negative float in input"""
+class TooMuchArgs(Exception):
+    """Exception for too much args"""
 
 def squareCalculator(floatNum):
     try:
         floatNum=float(floatNum)
         if floatNum < 0.0:
             raise NegativeFloatExcept
-        squareFloatNum=m.sqrt(floatNum)
+        else :
+            squareFloatNum=m.sqrt(floatNum)
+            return squareFloatNum
     except ValueError:
         print("BAD INPUT for " + str(floatNum))
+        return False
     except NegativeFloatExcept:
         print("BAD INPUT for " +str(floatNum))
+        return False
 
 def main(line):
-    floatList = line.split(";")
-    for floatNum in floatList:
-        squareCalculator(floatNum)
+    try:
+        if len(line.split(";"))>1:
+            raise TooMuchArgs
+        else:
+            floatNum=line
+            result=squareCalculator(floatNum)
+            if result == None:
+                return
+            else:
+                print(result)
+    except TooMuchArgs:
+        print("BAD INPUT - Too much args for this programme")
 
 if __name__=="__main__":
     try:
@@ -27,6 +42,6 @@ if __name__=="__main__":
         for line in inputFile:
             main(line)
     except IOError:
-            print("FILE NOT FOUND "+sys.arv[1] )
+            print("FILE NOT FOUND "+sys.argv[1] )
     except IndexError:
             print("BAD USAGE --> USAGE : python3 main.py inputfile")
--- ex02/test.py
+++ ex02/test.py
@@ -5,19 +5,28 @@ class TestNumberMethods(unittest.TestCase):
     """def testIsNotAString(self):
         self.assertFalse(squareCalculator("")"""
   
-    def testGlobalException(self):
-        self.assertRaise(ValueError,squareCalculator("string"))
+    def testStringException(self):
+        """with self.assertRaises(ValueError):
+            main.squareCalculator("string")"""
+        self.assertFalse(main.squareCalculator("string"))
 
-    def testSpecificException(self):
-        self.assertRaise(NegativeFloatExcept,squarecalculator(-10.2))
-    
+    def testNegativeIntException(self):
+        self.assertFalse(main.squareCalculator(-10.2))
+        """ with self.assertRaises(main.NegativeFloatExcept):
+            main.squareCalculator(-10.2)"""
+    def testInputWithInt(self):
+
+
     def testSquareEquality(self):
         testNumber=25.7
         expectedResult = math.sqrt(testNumber)
-        self.assertEquals(squareCalculator(testNumber),expectedResult)
+        self.assertEqual(main.squareCalculator(testNumber),expectedResult)
     
     def testSquareInequality(self):
         testNumber1=49.14
         testNumber2=104.26
         expectedResult2=math.sqrt(testNumber2)
-        self.assertNotEqual(squareCalculator(testNumber1),expectedResult2)
+        self.assertNotEqual(main.squareCalculator(testNumber1),expectedResult2)
+
+if __name__=="__main__":
+    unittest.main()
