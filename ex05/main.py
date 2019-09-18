d8ef750840072e2844c2406c047447a81a863583
Vacid <valentin.beysson@outlook.com>
2019-09-18 11:29:25 +0200

    Ex05, finger in the noose. Done.
    No need to create exception. In fact, no interaction with the INPUT.txt. 
    However, i kept it in the main.py (the program will use it anyway). 
    
    Tell me if you think i have to remove the part of the program where it 
    open the file

---
+++ ex05/main.py
@@ -0,0 +1,31 @@
+import os
+import sys
+
+
+def count():
+    for x in range(0,15,3):
+        print(x)
+    return 0
+
+def main(line):
+    try:
+        if len(line.split(";"))>1: ###Change number of args need on one line
+            raise TooMuchArgs
+        else:
+            var=line
+            result=count() ####Change function name and args
+            if result == None:
+                return
+        
+    except TooMuchArgs:
+        print("BAD INPUT - Too much args for this programme")
+
+if __name__=="__main__":
+    try:
+        inputFile=open(sys.argv[1],"r")
+        for line in inputFile:
+            main(line)
+    except IOError:
+            print("FILE NOT FOUND "+sys.argv[1] )
+    except IndexError:
+            print("BAD USAGE --> USAGE : python3 main.py inputfile")
