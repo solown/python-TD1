4387c0ee6dfe3bfc0fe3046c843ec27e18fc350f
Vacid <valentin.beysson@outlook.com>
2019-09-18 07:50:14 +0200

    Issue solve still wait for teacher answer

--- ex03/main.py.py
+++ ex03/main.py.py
@@ -3,49 +3,46 @@ class NoInputDetected(Exception):
 class NothingToCompare(Exception):
     pass
 
-resultat=""
+
 def comparaison_mot():
 
-        try:
-                mot1=(str(input("Entrer un mot: ")))
-                mot2=(str(input("Entrer un mot: ")))
+    mot1=(str(input("Entrer un mot: ")))
+    mot2=(str(input("Entrer un mot: ")))
+    if(mot1=="" or mot2==""):
+        raise NoInputDetected
+    if(mot1.lower() == mot2.lower()):
+        raise NothingToCompare
+    mot1l=mot1.lower()
+    mot2l=mot2.lower()
+    i=int(0)
+    trouver=False
+    lenght1=len(mot1)
+    lenght2=len(mot2)
+    try:
 
-                if(mot1=="" or mot2==""):
-                    raise NoInputDetected
-                if(mot1 == mot2):
-                    raise NothingToCompare
+        if lenght1>lenght2:
+            max=int(lenght2)
+        else:
+            max=int(lenght1)
+        while trouver == bool(False) and int(i)<int(max):
+        
+            if(ord(mot1l[i]) <= ord(mot2l[i]) ):
+                trouver = bool(True)
+                resultat = str(mot1)
+            elif(ord(mot1l[i]) >= ord(mot2l[i]) ):
+                trouver = bool(True)
+                resultat = str(mot2)
+            i+=1
+    except ValueError:
+        resultat="BAD INPUT"
+    except NoInputDetected:
+        print("Error: NoInputDetected\n *****comparaison_mot***** \n")
+        resultat="BAD INPUT"
+    except NothingToCompare:
+        print("It's the same string")
 
 
-                i=int(0)
-                trouver=False
-                lenght1=len(mot1)
-                lenght2=len(mot2)
-                if lenght1>lenght2:
-                    max=int(lenght2)
-                else:
-                    max=int(lenght1)
-
-                global resultat
-                while trouver == bool(False) and int(i)<int(max):
-
-                    if(ord(mot1[i]) < ord(mot2[i])):
-                        trouver = bool(True)
-                        resultat = mot1
-                    elif(ord(mot1[i]) > ord(mot2[i])):
-                        trouver = bool(True)
-                        resultat = mot2
-
-                    i+=1
-                
-        except ValueError:
-            resultat="BAD INPUT"
-        except NoInputDetected:
-            print("Error: NoInputDetected\n *****comparaison_mot***** \n")
-            resultat="BAD INPUT"
-        except NothingToCompare:
-            print("It's the same string")
-
-        return resultat
+    return resultat
 
 
 res=comparaison_mot()
