a1198d1f79c9b7a9f8969a5fa2af4ec38b455255
solown <thibaudrider@live.fr>
2019-09-23 11:48:46 +0200

    ex04 OK

--- ex04/main.py
+++ ex04/main.py
@@ -1,25 +1,30 @@
 import os
 import sys
 
+class NegativePressureInput(Exception):
+    """Negative pressure is not allowed"""
+class NegativeVolumeInput(Exception):
+    """Negative Volume is not allowed"""
+class TooMuchArgs(Exception):
+    """Too much args in line of input file"""
 
 def pressureChecker(pressure,volume):
     pSeuil = 2.3
     vSeuil = 7.41
-    try:
-        pressure=float(pressure)
-        volume=float(volume)
-        assert pressure<0.0
-        assert volume<0.0
-        if pressure > pSeuil && volume > vSeuil:
-            return "Arrêt immédiat"
-        elif pressure > pSeuil:
-            return "Diminuer le seuil"
-        elif volume > vSeuil:
-            return "Diminuer le volume"
-    except ValueError:
-        print("BAD INPUT")
-    except AssertionError:
-        print("BAD INPUT -> Seuil négatif")
+    pressure=float(pressure)
+    volume=float(volume)
+    if pressure<0.0:
+        raise NegativePressureInput
+    if volume<0.0:
+        raise NegativeVolumeInput
+    if pressure > pSeuil and volume > vSeuil:
+        return "Arrêt immédiat"
+    elif pressure > pSeuil:
+        return "Diminuer la pression"
+    elif volume > vSeuil:
+        return "Diminuer le volume"
+    else :
+        return "Tout va bien :)"
 
 def main(line):
     try:
@@ -34,7 +39,15 @@ def main(line):
                 print(result)
     except TooMuchArgs:
         print("BAD INPUT - Too much args for this programme")
-
+    except ValueError:
+        print("BAD INPUT")
+    except NegativePressureInput:
+        print("BAD INPUT -> Negative volume")
+    except NegativeVolumeInput:
+        print("BAD INPUT -> Negative pressure")
+    except IndexError:
+        print("BAD INPUT -> Two args needed per line")
+      
 if __name__=="__main__":
     try:
         inputFile=open(sys.argv[1],"r")
--- ex04/test.py
+++ ex04/test.py
@@ -2,11 +2,34 @@ import unittest
 import main
 
 class TestNumberMethods(unittest.TestCase):
-    def test1(self):
+    def testValueError(self):
+       with self.assertRaises(ValueError):
+            main.pressureChecker("string","string")
+       with self.assertRaises(ValueError):
+            main.pressureChecker("string","10.0")
+       with self.assertRaises(ValueError):
+            main.pressureChecker("10.0","string")
 
-    def test2(self):
+    def testNegativePressure(self):
+        with self.assertRaises(main.NegativePressureInput):
+            main.pressureChecker("-7.6","4.8")
 
-    .................
+    def testNegativeVolume(self):
+        with self.assertRaises(main.NegativeVolumeInput):
+            main.pressureChecker("7.6","-4.8")
 
-if __name__="__main__":
+    def testLowerPressure(self):
+        self.assertEqual(main.pressureChecker("2.6","5.6"),"Diminuer la pression")
+
+    def testLowerVolume(self):
+        self.assertEqual(main.pressureChecker("2.25","7.501"),"Diminuer le volume")
+
+    def testStopNow(self):
+        self.assertEqual(main.pressureChecker("2.34","7.501"),"Arrêt immédiat")
+
+    def testIsOkay(self):
+        self.assertEqual(main.pressureChecker("2.25","5.601"),"Tout va bien :)")
+                
+
+if __name__=="__main__":
     unittest.main()
