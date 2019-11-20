ec321d837b47437171abcc1a1fa3ca45aba18d16
Vacid <valentin.beysson@outlook.com>
2019-11-20 05:54:16 +0100

    PEP8 ex01 OK

--- ex01/main.py
+++ ex01/main.py
@@ -1,12 +1,12 @@
 import os
 import sys
 
+
 class NegativeInput(Exception):
     pass
 
 
 def calcul_vitesse(temps, distance):
-
 
     """
     Fonction qui calcul la vitesse.
@@ -22,56 +22,52 @@ def calcul_vitesse(temps, distance):
     """
     if (float(temps) == 0):
         raise ZeroDivisionError
-    elif (isinstance(float(temps),float) ==False or isinstance(float(distance),float)==False):
+    elif (isinstance(float(temps), float) is False or
+          isinstance(float(distance), float) is False):
         raise ValueError
     elif((float(temps) < 0) or (float(distance) < 0)):
         raise NegativeInput
     else:
-        temps=float(temps)
-        distance=float(distance)
-        vitesse=round(distance/temps,1)
-
-
+        temps = float(temps)
+        distance = float(distance)
+        vitesse = round(distance / temps, 1)
 
     return vitesse
 
 
 def main(line):
     try:
-
-        if len(line.split(";"))>2: ###Change number of args need on one line
+        if len(line.split(";")) > 2:
             raise TooMuchArgs
         else:
-            var=line
-            var=var.split(";")
-            temps=var[0]
-            distance=var[1]
+            var = line
+            var = var.split(";")
+            temps = var[0]
+            distance = var[1]
             try:
-
-
-                result=calcul_vitesse(temps,distance) ####Change function name and args
+                result = calcul_vitesse(temps, distance)
             except ZeroDivisionError:
                 print("Can't divide by 0:\n ex01.py \n ")
                 result = "BAD INPUT"
             except NegativeInput:
                 print("Negative Input detected")
-                result="BAD INPUT"
+                result = "BAD INPUT"
             except ValueError:
                 print("Somthing wrong with what you put in... \n ex01.py \n")
-                result="BAD INPUT"
-            if result == None:
+                result = "BAD INPUT"
+            if result is None:
                 return
             else:
-                print(result )
+                print(result)
     except TooMuchArgs:
         print("BAD INPUT - Too much args for this programme")
 
-if __name__=="__main__":
+if __name__ == "__main__":
     try:
-        inputFile=open(sys.argv[1],"r")
+        inputFile = open(sys.argv[1], "r")
         for line in inputFile:
             main(line)
     except IOError:
-            print("FILE NOT FOUND "+sys.argv[1] )
+            print("FILE NOT FOUND "+sys.argv[1])
     except IndexError:
             print("BAD USAGE --> USAGE : python3 main.py inputfile")
--- ex01/test.py
+++ ex01/test.py
@@ -1,15 +1,19 @@
 import unittest
 import main
 
+
 class TestNumberMethods(unittest.TestCase):
     def testZeroDivisionError(self):
         with self.assertRaises(ZeroDivisionError):
-            main.calcul_vitesse(0.0,5.0)
+            main.calcul_vitesse(0.0, 5.0)
+
     def testValueError(self):
         with self.assertRaises(ValueError):
-            main.calcul_vitesse("djflksd","ldkfjljf")
+            main.calcul_vitesse("djflksd", "ldkfjljf")
+
     def testNegativeInput(self):
         with self.assertRaises(main.NegativeInput):
-            main.calcul_vitesse(-3.0,-5.0)
-if __name__=="__main__":
+            main.calcul_vitesse(-3.0, -5.0)
+
+if __name__ == "__main__":
     unittest.main()
